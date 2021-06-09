0189d6b9bd9f20ea4e2ae6e6606d35cdf0c09a02
Coorple <52039908+Coorple@users.noreply.github.com>
2021-06-10 04:16:28 +0500

    fixing bugs

--- test_product_page.py
+++ test_product_page.py
@@ -63,9 +63,11 @@ def test_guest_can_go_to_login_page_from_product_page(browser):
 
 
 def test_guest_cant_see_product_in_basket_opened_from_product_page(browser):
-    link = 'http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/'
-    page = BasketPage(browser, link)
+    link = 'http://selenium1py.pythonanywhere.com/catalogue/the-city-and-the-stars_95/'
+    page = ProductPage(browser, link)
+    page.open()
     page.click_on_cart()
-    #page = BasketPage(browser, browser.current_url)
+    page = BasketPage(browser, browser.current_url)
     page.empty_car_check()
     page.empty_cart_message()
+
