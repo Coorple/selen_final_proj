c13a499cdd971a7767e444dff2e330a4cb290cca
Coorple <52039908+Coorple@users.noreply.github.com>
2021-06-09 06:14:32 +0500

    updated

--- test_main_page.py
+++ test_main_page.py
@@ -1,3 +1,6 @@
+import pytest
+
+from .pages.login_page import LoginPage
 from .pages.main_page import MainPage
 
 
@@ -6,6 +9,8 @@ def test_guest_can_go_to_login_page(browser):
     page = MainPage(browser, link)
     page.open()
     page.go_to_login_page()
+    login_page = LoginPage(browser, browser.current_url)
+    login_page.should_be_login_page()
 
 
 def test_guest_should_see_login_link(browser):
@@ -13,3 +18,10 @@ def test_guest_should_see_login_link(browser):
     page = MainPage(browser, link)
     page.open()
     page.should_be_login_link()
+
+
+#def test_login_form_should_be_presented(browser):
+   # link = "http://selenium1py.pythonanywhere.com/ru/accounts/login/"
+   # page = LoginPage(browser, link)
+   # page.open()
+   # page.should_be_login_page()
