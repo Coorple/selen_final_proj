e1382fa9de64cb304bc00233ad8554ed2f919c59
Coorple <52039908+Coorple@users.noreply.github.com>
2021-06-10 04:56:52 +0500

    Update test_main_page.py
    grouping 2 mainPage tests in class

--- test_main_page.py
+++ test_main_page.py
@@ -5,22 +5,21 @@ from .pages.login_page import LoginPage
 from .pages.main_page import MainPage
 
 
-@pytest.mark.skip()
-def test_guest_can_go_to_login_page(browser):
-    link = "http://selenium1py.pythonanywhere.com/"
-    page = MainPage(browser, link)
-    page.open()
-    page.go_to_login_page()
-    login_page = LoginPage(browser, browser.current_url)
-    login_page.should_be_login_page()
+@pytest.mark.login_guest
+class TestLoginFromMainPage:
+    def test_guest_can_go_to_login_page(self, browser):
+        link = "http://selenium1py.pythonanywhere.com/"
+        page = MainPage(browser, link)
+        page.open()
+        page.go_to_login_page()
+        login_page = LoginPage(browser, browser.current_url)
+        login_page.should_be_login_page()
 
-
-@pytest.mark.skip()
-def test_guest_should_see_login_link(browser):
-    link = "http://selenium1py.pythonanywhere.com/"
-    page = MainPage(browser, link)
-    page.open()
-    page.should_be_login_link()
+    def test_guest_should_see_login_link(self, browser):
+        link = "http://selenium1py.pythonanywhere.com/"
+        page = MainPage(browser, link)
+        page.open()
+        page.should_be_login_link()
 
 
 @pytest.mark.skip()
@@ -33,7 +32,7 @@ def test_login_form_should_be_presented(browser):
 
 def test_guest_cant_see_product_in_basket_opened_from_main_page(browser):
     link = 'http://selenium1py.pythonanywhere.com/'
-    page = MainPage(browser, link)  #I can initialize BasketPage from the start, but it's just don't seems right,
+    page = MainPage(browser, link)  # I can initialize BasketPage from the start, but it's just don't seems right,
     # so i first initialize MainPage object, while we on main page, and then , after we change pages,
     # i create new object, based on our new page
     page.open()
